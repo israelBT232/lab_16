19d8c12bcd840a798028313721d1a5166d5bcc54
israelBT232 <166003735+israelBT232@users.noreply.github.com>
2024-05-24 13:53:35 +0300

    Create Material.h

---
+++ libs/Materials/Material.h
@@ -0,0 +1,24 @@
+//
+// Created by MSI on 17.04.2024.
+//
+
+#ifndef LAB_16_0_UTIL_H
+#define LAB_16_0_UTIL_H
+#include <stdbool.h>
+
+int findMax(const int* array, int size);
+
+int findMin(const int* array, int size);
+
+bool isSet(const long long* array, int size);
+
+long long sum(const int* array, int size);
+
+int countUniques(long long* array, int size);
+
+bool isNonDescending(const int* array, int size);
+
+int countValues(const int* array, int size, int value);
+
+
+#endif //LAB_16_0_UTIL_H
